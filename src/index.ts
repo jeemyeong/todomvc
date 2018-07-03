41aa08bbbc107b99ec65b90a2a2ec632a95e8e67
Leo.fp <Leo.fp@kakaocorp.com>
2018-07-03 19:19:45 +0900

    Update: Complete first version by fp-ts

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,13 @@
 import { fromNullable, None, Option, Some } from 'fp-ts/lib/Option';
-import { curry } from "fp-ts/lib/function";
+import { curry, pipe } from "fp-ts/lib/function";
 
 const c = curry;
-const $ = (selector: string, element: Element | Document = document) => fromNullable(element.querySelector(selector));
+const $ = (selector: string, element: Element | Document = document) => element.querySelectorAll(selector);
 const addEventListener = (type: string, fn: any) => (element: EventTarget) => element.addEventListener(type, fn) || element;
 const isEnter = (e: KeyboardEvent) => e.keyCode == 13;
 const verifyKeyboardEvent = (e: KeyboardEvent) =>
     isEnter(e) && (e.target as HTMLInputElement).value.length > 0;
-const eventToOption = (e: KeyboardEvent, pred: (e: KeyboardEvent) => boolean = () => true) => {
-  e.preventDefault();
+const eventToOption = (pred: (e: KeyboardEvent) => boolean) => (e: KeyboardEvent) => {
   return (pred(e) ? new Some(e) : None.value);
 };
 const keyboardEventToValue = (e: KeyboardEvent) => (e.target as HTMLInputElement).value;
@@ -17,31 +16,32 @@ const createElem = (tagName: string, template: string) => {
   elem.innerHTML = template;
   return elem
 };
-
 const tap = <T, U>(fn: (x: U) => T) => (x: U): U => {
   fn(x);
   return x;
 };
-
 // side effect
 const appendLi = (elem: Element, ul: Element) => {
   ul.appendChild(elem);
   return elem;
 };
-
 const getTarget = (e: Event) => (e.target as Element);
-const isEventTarget = (e: KeyboardEvent, tagName: string, className: string ) =>
-  getTarget(e).tagName.toLowerCase() == tagName && getTarget(e).classList.contains(className);
+const isEventTarget = (e: Event, tagName: string, className?: string) =>
+  getTarget(e).tagName.toLowerCase() == tagName.toLowerCase() && (!className || getTarget(e).classList.contains(className));
 
+// side effect
 const toggleClassIntoElem = (className: string) => (elem: Element) => {
   elem.classList.contains(className) ? elem.classList.remove(className) : elem.classList.add(className);
   return elem
 };
+const getFirstNullable = <T>(list: T) => fromNullable(list[0]);
+
+const getInnerHtml = (elem: Element) =>
+  elem.innerHTML;
 
 (function () {
-  (function todoApp(todoApp: Option<Element>) {
-    (function addTodo(todoList: Option<Element>) {
-      const todoTemplate = (todo: string) => (`
+  (function(todoApp: Option<Element>) {
+    const todoTemplate = (todo: string) => (`
         <div class="view">
           <input class="toggle" type="checkbox">
           <label>${todo}</label>
@@ -49,61 +49,197 @@ const toggleClassIntoElem = (className: string) => (elem: Element) => {
         </div>
       `);
 
-      // side effect
-      const clearTextInput = (x: any) => todoApp.chain(c($)('input.new-todo')).map(input => {
-        input["value"] = '';
-        return x
-      });
+    const todoCountTemplate = (count: number) =>
+      `<strong>${count}</strong> item left</span>`;
 
-      // side effect
-      const appendIntoTodoList = (todoElem: Element) =>
+    (function(todoList: Option<Element>){
+
+      (function addTodo(todoList: Option<Element>) {
+        // side effect
+        const clearInput = () =>
+          todoApp
+            .map(c($)('input.new-todo'))
+            .chain(getFirstNullable)
+            .map(input => input["value"] = '');
+
+        // side effect
+        const appendTodoElem = (todoElem: Element) =>
+          todoList
+            .map( c(appendLi)(todoElem) )
+            .map( tap(clearInput) );
+
+        const createTodoElem = (todo: string) => createElem("li", todoTemplate(todo));
+
+        const addTodo = (e: KeyboardEvent) =>
+          fromNullable(e)
+            .chain(eventToOption(verifyKeyboardEvent))
+            .map(keyboardEventToValue)
+            .map(createTodoElem)
+            .map(tap(appendTodoElem))
+            .map(tap(() => updateTodoCount(getTodoCountNullable())));
+
+        todoApp
+          .map(addEventListener('keydown', addTodo));
+
+      })(todoApp.chain(elem => getFirstNullable($('ul.todo-list', elem))));
+
+
+      (function deleteTodo(todoList: Option<Element>){
+        const filterEventTarget = (e: Event) =>
+          fromNullable(e)
+            .filter(e => (isEventTarget(e, "button", "destroy")))
+            .map(tap(console.log))
+            .map(getTarget);
+
+        const deleteTodo = (target: Option<Element>) =>
+          target
+            .map(target => target.closest("li"))
+            .filter(elem => confirm(`Delete?`))
+            // side effect
+            .map(elem => elem.remove())
+            .map(tap(() => updateTodoCount(getTodoCountNullable())));
+
         todoList
-          .map( c(appendLi)(todoElem) )
-          .map( tap(clearTextInput) );
-      const createTodoElem = (todo: string) =>
-        createElem("li", todoTemplate(todo));
+          .map(addEventListener('click', pipe(filterEventTarget, deleteTodo)));
 
-      const addTodo = (e: KeyboardEvent) =>
+      })(todoApp.chain(elem => getFirstNullable($('ul.todo-list', elem))));
+
+      const getAllTodoList = () =>
+        todoList
+          .map(c($)('li'))
+          .map(lst => Array.from<Element>(lst));
+
+      const getCompletedList = (list: Array<Element>) =>
+        list.filter(li => (li["classList"] as DOMTokenList).contains("completed"));
+
+      const getUncompletedList = (list: Array<Element>) =>
+        list.filter(li => !(li["classList"] as DOMTokenList).contains("completed"));
+
+      const filterEventTarget = (e: Event) =>
         fromNullable(e)
-          .chain(e => eventToOption(e, verifyKeyboardEvent))
-          .map(keyboardEventToValue)
-          .map(createTodoElem)
-          .map(tap(appendIntoTodoList));
-
-      todoApp
-        .map(addEventListener('keyup', addTodo));
-
-    })(todoApp.chain(c($)('ul.todo-list')));
-
-
-    (function deleteTodo(todoList: Option<Element>){
-      const deleteTodo = (e: KeyboardEvent) =>
-        fromNullable(e)
-          .chain(eventToOption)
-          .filter(e => (isEventTarget(e, "button", "destroy")))
-          .map(getTarget)
-          .map(target => target.closest("li"))
-          .filter(elem => confirm(`Delete?`))
-          .map(elem => elem.remove())
-      ;
-
-      todoList
-        .map(addEventListener('click', deleteTodo));
-    })(todoApp.chain(c($)('ul.todo-list')));
-
-    (function checkTodo(todoList: Option<Element>){
-      const checkTodo = (e: KeyboardEvent) =>
-        fromNullable(e)
-          .chain(eventToOption)
           .filter(e => (isEventTarget(e, "input", "toggle")))
-          .map(getTarget)
+          .map(getTarget);
+
+      const checkTodo = (target: Option<Element>) =>
+        target
           .map(target => target.closest("li"))
+          // side effect
           .map(toggleClassIntoElem("completed"))
-      ;
+          .map(tap(() => updateTodoCount(getTodoCountNullable())));
 
       todoList
-        .map(addEventListener('click', checkTodo));
-    })(todoApp.chain(c($)('ul.todo-list')));
+        .map(addEventListener('click', pipe(filterEventTarget, checkTodo)));
 
-  })($('.todoapp'))
+      (function checkAllTodo(label: Option<Element>) {
+
+        const toggleCheckbox = (li: Element) =>
+          getFirstNullable($("input.toggle", li))
+            .map(input => (input as HTMLInputElement).checked = !(input as HTMLInputElement).checked);
+
+        const checkAllTodo = () =>
+          getAllTodoList()
+            .map(list => {
+              const completedList = getCompletedList(list);
+              if (completedList.length < list.length) {
+                return getUncompletedList(list);
+              }
+              return completedList;
+            })
+            .map(tap(list =>
+              list.forEach((li: Element) =>
+                checkTodo(fromNullable(li))
+                  .map(toggleCheckbox))));
+
+        label
+          .map(addEventListener('click', checkAllTodo));
+      })(todoApp.chain(elem => getFirstNullable($('label[for=toggle-all]', elem))));
+
+      (function filter(filters: Option<Element>) {
+
+        enum Filter {
+          All = "All",
+          Active = "Active",
+          Completed = "Completed"
+        };
+
+        const hiddenElem = (elem: HTMLElement) => elem.style.display = "none";
+        const showElem = (elem: HTMLElement) => elem.style.display = "";
+
+        const filterTodo = (filter: Filter) => {
+          switch (filter) {
+            case Filter.All:
+              getAllTodoList()
+                .map(li => {
+                  // side effect
+                  li.forEach(showElem)
+                });
+              break;
+            case Filter.Completed:
+              getAllTodoList()
+                .map(list => {
+                  // side effect
+                  getCompletedList(list).forEach(showElem);
+                  getUncompletedList(list).forEach(hiddenElem);
+                });
+              break;
+            case Filter.Active:
+              getAllTodoList()
+                .map(list => {
+                  // side effect
+                  getCompletedList(list).forEach(hiddenElem);
+                  getUncompletedList(list).forEach(showElem);
+                });
+              break;
+          }
+        };
+
+        const filterEvent = (e: Event) =>
+          fromNullable(e)
+            .filter(e => (isEventTarget(e, "a")))
+            .map(getTarget)
+            .map(tap(
+              target =>
+                filters
+                  .map(filters => $('a', filters))
+                  .map(filter => Array.from<Element>(filter))
+                  .map(anchors =>
+                    anchors
+                      .filter(anchor => anchor != target)
+                      .forEach(anchor => anchor.classList.remove("selected")))
+            ))
+            .map(tap(toggleClassIntoElem("selected")))
+            .map(getInnerHtml)
+            .chain(filter => filter in Filter ? new Some(filter) : None.value)
+            .map(tap(filterTodo));
+
+        filters
+          .map(addEventListener('click', filterEvent))
+      }(todoApp.chain(elem => getFirstNullable($('ul.filters', elem)))));
+
+      (function clearComplete(clearCompleteBtn: Option<Element>) {
+        const clearComplete = () =>
+          getAllTodoList()
+            .map(list => {
+              getCompletedList(list).forEach(
+                li => (li as Element).remove()
+              );
+            });
+
+        clearCompleteBtn
+          .map(addEventListener('click', clearComplete))
+      }(todoApp.chain(elem => getFirstNullable($('button.clear-completed', elem)))));
+
+      const updateTodoCount = (todoCount: Option<Element>) =>
+        getAllTodoList()
+          .map(getUncompletedList)
+          .map(lst => lst.length)
+          .map(count =>
+            todoCount.map(todoCount => todoCount.innerHTML = todoCountTemplate(count))
+          );
+      const getTodoCountNullable = () => (todoApp.chain(elem => getFirstNullable($('.todo-count', elem))));
+      updateTodoCount(getTodoCountNullable());
+
+    })(todoApp.chain(elem => getFirstNullable($('ul.todo-list', elem))));
+
+  })(getFirstNullable($('.todoapp')))
 })();
