deec077fbc02bc35e33fd23e08a7fa50374d36fd
Leo.fp <Leo.fp@kakaocorp.com>
2018-07-03 13:18:47 +0900

    Update: delete todo and toggle todo

--- src/index.ts
+++ src/index.ts
@@ -7,9 +7,36 @@ const addEventListener = (type: string, fn: any) => (element: EventTarget) => el
 const isEnter = (e: KeyboardEvent) => e.keyCode == 13;
 const verifyKeyboardEvent = (e: KeyboardEvent) =>
     isEnter(e) && (e.target as HTMLInputElement).value.length > 0;
-const eventToOption = (e: KeyboardEvent) =>
-    (verifyKeyboardEvent(e) ? new Some(e) : None.value);
+const eventToOption = (e: KeyboardEvent) => {
+  e.preventDefault();
+  return (verifyKeyboardEvent(e) ? new Some(e) : None.value);
+};
 const keyboardEventToValue = (e: KeyboardEvent) => (e.target as HTMLInputElement).value;
+const createElem = (tagName: string, template: string) => {
+  const elem = document.createElement(tagName);
+  elem.innerHTML = template;
+  return elem
+};
+
+const tap = <T, U>(fn: (x: U) => T) => (x: U): U => {
+  fn(x);
+  return x;
+};
+
+// side effect
+const appendLi = (elem: Element, ul: Element) => {
+  ul.appendChild(elem);
+  return elem;
+};
+
+const getTarget = (e: Event) => (e.target as Element);
+const isEventTarget = (e: KeyboardEvent, tagName: string, className: string ) =>
+  getTarget(e).tagName.toLowerCase() == tagName && getTarget(e).classList.contains(className);
+
+const toggleClassIntoElem = (className: string) => (elem: Element) => {
+  elem.classList.contains(className) ? elem.classList.remove(className) : elem.classList.add(className);
+  return elem
+};
 
 (function () {
   (function todoApp(todoApp: Option<Element>) {
@@ -22,32 +49,59 @@ const keyboardEventToValue = (e: KeyboardEvent) => (e.target as HTMLInputElement
         </div>
       `);
 
-      const createTodoElem = (todo: string) => {
-        const elem = document.createElement('li');
-        elem.innerHTML = todoTemplate(todo);
-        return elem
-      };
-
       // side effect
-      const appendLi = (todoElem: Element, ul: Element) => {
-        ul.appendChild(todoElem);
-        return todoElem;
-      };
+      const clearTextInput = (x: any) => todoApp.chain(c($)('input.new-todo')).map(input => {
+        input["value"] = '';
+        return x
+      });
 
       // side effect
       const appendIntoTodoList = (todoElem: Element) =>
-        todoList.map( c(appendLi)(todoElem) )
+        todoList
+          .map( c(appendLi)(todoElem) )
+          .map( tap(clearTextInput) );
+      const createTodoElem = (todo: string) =>
+        createElem("li", todoTemplate(todo));
 
       const addTodo = (e: KeyboardEvent) =>
         fromNullable(e)
           .chain(eventToOption)
           .map(keyboardEventToValue)
           .map(createTodoElem)
-          .chain(appendIntoTodoList)
-          .map(console.log);
+          .map(tap(appendIntoTodoList));
 
       todoApp
         .map(addEventListener('keyup', addTodo));
-    })(todoApp.chain(c($)('ul.todo-list')))
+
+    })(todoApp.chain(c($)('ul.todo-list')));
+
+
+    (function deleteTodo(todoList: Option<Element>){
+      const deleteTodo = (e: KeyboardEvent) =>
+        fromNullable(e)
+          .filter(e => (isEventTarget(e, "button", "destroy")))
+          .map(getTarget)
+          .map(target => target.closest("li"))
+          .filter(elem => confirm(`Delete?`))
+          .map(elem => elem.remove())
+      ;
+
+      todoList
+        .map(addEventListener('click', deleteTodo));
+    })(todoApp.chain(c($)('ul.todo-list')));
+
+    (function checkTodo(todoList: Option<Element>){
+      const checkTodo = (e: KeyboardEvent) =>
+        fromNullable(e)
+          .filter(e => (isEventTarget(e, "input", "toggle")))
+          .map(getTarget)
+          .map(target => target.closest("li"))
+          .map(toggleClassIntoElem("completed"))
+      ;
+
+      todoList
+        .map(addEventListener('click', checkTodo));
+    })(todoApp.chain(c($)('ul.todo-list')));
+
   })($('.todoapp'))
 })();
